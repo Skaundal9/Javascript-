6efadb38424fa10882db5ced4f897863ce39f2fe
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-27 16:44:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122341 //Cant be changed (Constant)
+let accountEmail = "shubham123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+/*
+var keyword is not preffered for declaring variable, as it is set globally not for only a particular block of code
+*/
+// accountId=2  Not Acceptable
+accountEmail="sachin@gmail.com"
+accountPassword="2345"
+accountCity="Mumbai"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
