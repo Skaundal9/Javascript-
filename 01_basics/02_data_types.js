0db0819fbbf4b5dd87137db5d0e3abf29f8d7128
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-27 16:44:12 +0000

    data_types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,12 @@
+"use strick"; // treat all js code as newer version
+// alert(3+3) //we are using node.js not browser
+// Code readibility should be high
+
+let name="Shubham"
+let age=21
+let isLoggedIn=true
+let state=null // standalone value
+
+console.log(typeof null)
+console.log(typeof undefined)
+
