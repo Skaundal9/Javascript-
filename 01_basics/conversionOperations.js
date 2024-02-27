05d1033bd65a8a9e1a960a38bf626f95740f27b9
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-27 17:38:34 +0000

    conversionOperations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -19,5 +19,27 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 
 let isNumber = 33
 let stringNumber=String(isNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+//console.log(stringNumber)
+
+
+// ********************* Operations **********//
+let value=3
+let negValue= -value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2*2);
+let str1="Hello"
+let str2=" Shubham"
+let str3=str1 + str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(+true)
+// console.log(+"")
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+
