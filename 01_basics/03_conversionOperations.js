ec4c6632efbcbf080f10bb8fcef615497dd4c3af
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-28 03:24:30 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,46 @@
+let score = "Shubham"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+// "33" => 33
+// "33abc"=>NaN
+// true=>1; false=>0
+let isLoggedIn = "Shubham"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=>true; o=>false
+// "" => false
+// "Shubham" = true
+
+let isNumber = 33
+let stringNumber=String(isNumber)
+// console.log(typeof stringNumber)
+//console.log(stringNumber)
+
+
+// ********************* Operations **********//
+let value=3
+let negValue= -value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2*2);
+let str1="Hello"
+let str2=" Shubham"
+let str3=str1 + str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(+true)
+// console.log(+"")
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+
+
