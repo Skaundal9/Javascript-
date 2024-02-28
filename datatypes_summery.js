18f4fda53f8490453382fa528f011fab5d21eecb
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-28 03:58:07 +0000

    datatypes_summery

---
+++ datatypes_summery.js
@@ -0,0 +1,31 @@
+//Primitive(call by value)
+// 7 types: String,Number, Boolean, null, undefined, Symbol,BigInt
+
+const oustiseTemp=null
+let userEmail;
+
+//Symbol
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+//BigInt
+//const bigNumber=22553625656n
+
+// Non-Primitive(call by reference)
+// Array, Objects,Functions
+
+//Array
+const heros=["shaktiman","naagraj","doga"];
+
+//Object
+let myObj={
+    name:"Shubham",
+    age:22,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello Shubham")
+}
+console.log(typeof myFunction)
