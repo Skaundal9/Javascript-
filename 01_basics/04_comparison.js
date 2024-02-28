239c19cc031e962e57f2429e7b161ae0b549b8b3
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-28 03:24:44 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+
+// console.log("2">1)
+// console.log("02">1)
+
+// These type of comprison is not used according to standards
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+
+// Strict Check (===) checks equality as well as data type
