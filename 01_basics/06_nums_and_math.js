7819a1515e2c97123e0a339ce089e3650a402533
Shubham Kaundal <103916684+Skaundal9@users.noreply.github.com>
2024-02-29 09:58:55 +0000

    nums_and_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -26,4 +26,9 @@ const hundreds = 1000000
 // console.log(Math.max(7,6,9,4));
 
 console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1)
 
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min) //Understand again
